24b03a43937e1edf85e177ad32eb78d9b482ebc3
Hamid Taheri <h.taheri@sadad.co.ir>
2024-01-16 04:42:00 +0330

    SSD-2091 #time 15m init(emp): add empty library

---
+++ projects/organization-chart/src/lib/organization-chart.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-organization-chart',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      organization-chart works!
+    </p>
+  `,
+  styles: ``
+})
+export class OrganizationChartComponent {
+
+}
---
+++ projects/organization-chart/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of organization-chart
+ */
+
+export * from './lib/organization-chart.service';
+export * from './lib/organization-chart.component';
